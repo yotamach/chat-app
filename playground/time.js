e7c3ea3c14c6a8309868c001de70994cf05aa8a6
Yotam Achrak <yotam2585@gmail.com>
2018-09-03 12:26:53 +0300

    format timestamp using moment

---
+++ playground/time.js
@@ -0,0 +1,7 @@
+const moment = require('moment');
+
+let date = moment();
+
+//date.add(1,'years').subtract(9,'days');
+
+console.log(moment().valueOf());
--- server/utils/message.js
+++ server/utils/message.js
@@ -1,8 +1,10 @@
+const moment = require('moment');
+
 let generateMessage = (from,text) => {
     return {
         from,
         text,
-        createAt: new Date().getTime()
+        createAt: moment().valueOf()
     }
 };
 
@@ -10,7 +12,7 @@ let generateLocationMessage = (from,latitude,longitude) => {
     return {
         from,
         url: `https://www.google.com/maps?q=${latitude},${longitude}`,
-        createAt: new Date().getTime()
+        createAt: moment().valueOf()
     }
 };
 
